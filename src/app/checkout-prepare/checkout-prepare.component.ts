cb2b6f7ba89b05012c75a11ea9fcc2c3fd010a33
Santhosh Balaguru <santhosh.balaguru@thoughthub.net>
2019-01-25 19:00:17 +0530

    newchanges

--- src/app/checkout-prepare/checkout-prepare.component.ts
+++ src/app/checkout-prepare/checkout-prepare.component.ts
@@ -8,22 +8,13 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http/'
   styleUrls: ['./checkout-prepare.component.css']
 })
 export class CheckoutPrepareComponent implements OnInit {
-  userName:string = " ";
-  res: any;
+ 
   constructor(private http :HttpClient) { }
 
   ngOnInit() {
    
     };
-    search(){
-      let obs = this.http.get("http://publicapi.eventxpress.net/v2/api/checkout/prepare/2138990184/2138982482" + this.userName);
-
-      obs.subscribe((res)=>{
-        this.res= res;
-        console.log(res);
-      })
-      
-    }
+    
    
     
   }
--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http/';
+import { Event } from 'src/models/Event';
+import { Observable } from 'rxjs';
+
+
+let mytoken = "565e95b08ad5210001000001b0db86dc97f6477c781493729c6cbcfb";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-api-key': mytoken
+  })
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class EventService {
   printToConsole(arg){
     console.log(arg);
   }
-  constructor() { }
-}
+  public api_url:string = "http://publicapi.eventxpress.net/v2/api/checkout/prepare/2138990184/2138982482";
+  
+  constructor(private http: HttpClient) { };
+   
+  getUsers():Observable<Event[]>
+  {
+    return this.http.get<Event[]>(`${this.api_url}`,httpOptions);
+  }
+
+  }
+
